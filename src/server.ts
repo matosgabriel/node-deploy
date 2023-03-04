67fa015042a07f79f6edba220ed6db6dc388a81e
Gabriel Matos <gabriel.ifsal@gmail.com>
2023-03-04 11:07:31 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+
+const app = fastify();
+const prismaClient = new PrismaClient();
+
+app.get('/users', async () => {
+  const users = await prismaClient.user.findMany();
+
+  return { users }
+});
+
+app.post('/users', async (request, reply) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email()
+  })
+
+  const { name, email } = createUserSchema.parse(request.body);
+
+  await prismaClient.user.create({ data: { name, email } });
+
+  return reply.status(201).send();
+});
+
+app.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333
+}).then(() => console.log(`HTTP Server running at ${process.env.PORT || 3333}!`));
